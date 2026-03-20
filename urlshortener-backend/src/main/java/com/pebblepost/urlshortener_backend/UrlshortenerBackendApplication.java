136b4eb5b2d7572e453dad24ba0f115a42447704
Shivam1615 <Shivam1615@gmail.com>
2026-03-19 23:08:44 -0400

    export PATH="/usr/local/opt/mysql-client/bin:$PATH"

---
+++ urlshortener-backend/src/main/java/com/pebblepost/urlshortener_backend/UrlshortenerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pebblepost.urlshortener_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlshortenerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlshortenerBackendApplication.class, args);
+	}
+
+}
---
+++ urlshortener-backend/src/test/java/com/pebblepost/urlshortener_backend/UrlshortenerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pebblepost.urlshortener_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlshortenerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
